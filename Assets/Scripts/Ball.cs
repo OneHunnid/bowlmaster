a8b281f2fe4fc01aaa6030bf8f623931ca1dbe44
Chris <climbrick1989@gmail.com>
2017-05-19 14:51:53 -0400

    Finish lecture 195

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-	public float launchSpeed;
+	public Vector3 launchVelocity;
 
 	private Rigidbody rigidBody;
 	private AudioSource audioSource;
@@ -11,18 +11,14 @@ public class Ball : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.useGravity = false;
+	}
+
+	public void Launch (Vector3 velocity) {
+		rigidBody.useGravity = true;
+		rigidBody.velocity = velocity;
+
 		audioSource = GetComponent<AudioSource> ();
-
-		Launch ();
-	}
-
-	public void Launch () {
-		rigidBody.velocity = new Vector3 (0, 0, launchSpeed);
 		audioSource.Play ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 }
